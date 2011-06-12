bd5fa77149fa69bed420906f96d19264d6fb1d59
nnydjesus@gmail.com <nnydjesus@gmail.com@ad20a0ba-3b03-c38a-b13f-b6fcbae1c783>
2011-06-12 01:11:33 +0000

    algo de los warnings

--- dopplereffect/dopplereffect-service/src/main/java/ar/edu/unq/dopplereffect/service/ServiceImpl.java
+++ dopplereffect/dopplereffect-service/src/main/java/ar/edu/unq/dopplereffect/service/ServiceImpl.java
@@ -7,13 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.unq.dopplereffect.repositories.Repository;
 
-/**
- * 
- * XXX este modo de crear servicios tiene el gran problema que no pueden hacer
- * validaciones de negocio en servicios o chequeos en general, si un add de un
- * objeto requiero validar precondiciones y contruir un objeto mas complejo
- * antes de guardarlo, no s seria posible
- */
 public class ServiceImpl<T> implements PersistenceService<T>, Serializable {
 
     private static final long serialVersionUID = -806879556462184789L;
@@ -52,7 +45,7 @@ public class ServiceImpl<T> implements PersistenceService<T>, Serializable {
     @Override
     @Transactional
     public List<T> searchByExample(final T object) {
-    	return repository.searchByExample(object);
+        return repository.searchByExample(object);
     }
 
     public void setRepository(final Repository<T> repository) {
