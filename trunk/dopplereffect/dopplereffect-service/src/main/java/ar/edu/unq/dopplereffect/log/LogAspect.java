017585ad6d28efec1329e343400f2e2ddbd2f26c
nnydjesus@gmail.com <nnydjesus@gmail.com@ad20a0ba-3b03-c38a-b13f-b6fcbae1c783>
2011-07-03 20:45:33 +0000

    aspecto de auditoria, servicio de mail, y servicio de reporting (exportar a pdf, rtf y xls)

--- trunk/dopplereffect/dopplereffect-service/src/main/java/ar/edu/unq/dopplereffect/log/LogAspect.java
+++ trunk/dopplereffect/dopplereffect-service/src/main/java/ar/edu/unq/dopplereffect/log/LogAspect.java
@@ -5,21 +5,39 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+import ar.edu.unq.dopplereffect.exceptions.UserException;
+import ar.edu.unq.dopplereffect.service.mail.MailService;
+
 /**
  */
 @Aspect
 public class LogAspect {
-	
-	private static final Logger LOG = Logger.getLogger(LogAspect.class);
+
+    private static final Logger LOG = Logger.getLogger(LogAspect.class);
+
+    private MailService mailService;
 
     @Around("execution(* ar.edu.unq.dopplereffect.service.*.*.*(..))")
     public Object aroundSearch(final ProceedingJoinPoint pjp) throws Throwable {
-    	try {
-    		LOG.debug(pjp.getTarget() + " execute "+pjp.getSignature().getName());
-    		return pjp.proceed();
-		} catch (Exception e) {
-    		LOG.error(pjp.getTarget() + " execute "+pjp.getSignature().getName()+ " with error "+e);
-			throw e;
-		}
+        try {
+            LOG.info(pjp.getTarget() + " execute " + pjp.getSignature().getName());
+            return pjp.proceed();
+        } catch (UserException e) {
+            LOG.error(pjp.getTarget() + " execute " + pjp.getSignature().getName() + " with error " + e);
+            this.getMailService().sendErrorMail(e, "nnydjesus@gmail.com");
+            throw e;
+        } catch (Exception e) {
+            LOG.error(pjp.getTarget() + " execute " + pjp.getSignature().getName() + " with error " + e);
+            this.getMailService().sendErrorMail(e, "nnydjesus@gmail.com");
+            throw e;
+        }
+    }
+
+    public void setMailService(final MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    public MailService getMailService() {
+        return mailService;
     }
 }
---
+++ trunk/dopplereffect/dopplereffect-service/src/main/java/ar/edu/unq/dopplereffect/service/export/FormatterExportType.java
@@ -0,0 +1,23 @@
+package ar.edu.unq.dopplereffect.service.export;
+
+/**
+ * TODO: description
+ */
+public enum FormatterExportType {
+    PDF(".pdf"), RTF(".rtf"), EXCEL(".xls");
+
+    private String extension;
+
+    private FormatterExportType(final String value) {
+        extension = value;
+    }
+
+    public void setExtension(final String extension) {
+        this.extension = extension;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+}
